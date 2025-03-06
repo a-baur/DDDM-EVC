fixed loss computation when using mixup

src/models/dddm/dddm.py
@@ -35,14 +35,16 @@ class DDDM(nn.Module):
         :return: Tuple of the diffusion loss and the reconstruction loss
         """
         mixup_ratios = torch.randint(0, 2, (x.batch_size,)).to(x.device).detach()
-        src_mel, ftr_mel = self.encoder(x, g, mixup_ratios)
+        src_mel, ftr_mel, src_mixup, ftr_mixup = self.encoder(x, g, mixup_ratios)
 
         max_length_new = util.get_u_net_compatible_length(x.mel.size(-1))
-        src_mel, ftr_mel, x.mel, x_mask = util.pad_tensors_to_length(
-            [src_mel, ftr_mel, x.mel, x.mask], max_length_new
+        src_mixup, ftr_mixup, x_pad, x_mask_pad = util.pad_tensors_to_length(
+            [src_mixup, ftr_mixup, x.mel, x.mask], max_length_new
         )
 
-        diff_loss = self.diffusion.compute_loss(x.mel, x_mask, src_mel, ftr_mel, g)
+        diff_loss = self.diffusion.compute_loss(
+            x_pad, x_mask_pad, src_mixup, ftr_mixup, g
+        )
         rec_loss = F.l1_loss(x.mel, src_mel + ftr_mel)
         return diff_loss, rec_loss
 

src/modules/wavenet_decoder/modules.py
@@ -168,7 +168,10 @@ class WavenetDecoder(nn.Module):
         x: DDDMInput,
         g: torch.Tensor,
         mixup_ratios: torch.Tensor = None,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
+    ) -> (
+        tuple[torch.Tensor, torch.Tensor]
+        | tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]
+    ):
         """
         Forward pass of the Source-Filter encoder.
 
@@ -176,6 +179,7 @@ class WavenetDecoder(nn.Module):
         :param g: Global conditioning tensor
         :param mixup_ratios: Mixup ratios for each sample in the batch
         :return: Source, and Filter representations
+            If mixup_ratios is not None, return the mixed up outputs as well
         """
         content = self.emb_c(x.emb_content)
         f0 = self.emb_f0(x.emb_pitch).transpose(1, 2)
@@ -198,16 +202,18 @@ class WavenetDecoder(nn.Module):
 
             # Mixup the outputs according to the mixup ratio
             mixup_ratios = mixup_ratios[:, None, None]  # (B) -> (B x 1 x 1)
-            y_src = (
+            y_src_mixup = (
                 mixup_ratios * y_src[:batch_size, :, :]
                 + (1 - mixup_ratios) * y_src[batch_size:, :, :]
             )
-            y_ftr = (
+            y_ftr_mixup = (
                 mixup_ratios * y_ftr[:batch_size, :, :]
                 + (1 - mixup_ratios) * y_ftr[batch_size:, :, :]
             )
+            y_src_true = y_src[:batch_size, :, :]
+            y_ftr_true = y_ftr[:batch_size, :, :]
+            return y_src_true, y_ftr_true, y_src_mixup, y_ftr_mixup
         else:
             y_ftr = self.dec_ftr(F.relu(content), x.mask, g=g)
             y_src = self.dec_src(f0, x.mask, g=g)
-
-        return y_src, y_ftr
+            return y_src, y_ftr