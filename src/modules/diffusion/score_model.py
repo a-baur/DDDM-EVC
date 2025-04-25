5c6bd749f39b6be2e585d6508376e255365f3f84
a-baur <alexba.baur@protonmail.com>
2025-04-25 12:55:48 +0200

    conditioning experiments

--- src/modules/diffusion/score_model.py
+++ src/modules/diffusion/score_model.py
@@ -166,7 +166,7 @@ class TokenScoreEstimator(torch.nn.Module):
     ) -> None:
         super(TokenScoreEstimator, self).__init__()
 
-        dims = [1 + dim_cond, *map(lambda m: dim_base * m, dim_mults)]
+        dims = [2 + dim_cond, *map(lambda m: dim_base * m, dim_mults)]
         in_out = list(zip(dims[:-1], dims[1:]))
 
         self.time_pos_emb = SinusoidalPosEmb(dim_base)
@@ -181,6 +181,7 @@ class TokenScoreEstimator(torch.nn.Module):
             Mish(),
             torch.nn.Conv1d(4 * dim_cond, dim_cond, 1),
         )
+        self.cond_emb = torch.nn.Conv1d(gin_channels, 80, 1)  # [batch, dim_cond, time]
 
         self.downs = torch.nn.ModuleList([])
         self.ups = torch.nn.ModuleList([])
@@ -240,8 +241,8 @@ class TokenScoreEstimator(torch.nn.Module):
         condition = self.time_pos_emb(t)
         t = self.mlp(condition)
 
-        # x = torch.stack([x, enc_out], 1) # [batch, enc_out, x_channel, time]
-        x = x[:, None, :, :]  # [batch, 1, x_channel, time]
+        g_emb = self.cond_emb(g)
+        x = torch.stack([x, g_emb], 1)  # [batch, enc_out, x_channel, time]
         x_mask = x_mask.unsqueeze(1)
 
         condition = condition.unsqueeze(-1).expand(-1, -1, g.size(-1))
