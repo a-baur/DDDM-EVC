cc85ef2b247bd7fe67c693ae199a43dbe3987a67
a-baur <alexba.baur@protonmail.com>
2025-04-26 13:23:30 +0200

    added snr weight clamping

--- src/models/token_diffusion.py
+++ src/models/token_diffusion.py
@@ -221,14 +221,21 @@ class TokenDiffusion(torch.nn.Module):
         z_estimation *= torch.sqrt(1.0 - self.get_gamma(0, t, p=2.0, use_torch=True))
 
         alpha_t: torch.Tensor = self.get_gamma(0, t, p=2.0, use_torch=True)
-        snr_weight = alpha_t / (1.0 - alpha_t + 1e-5)
-        snr_weight = snr_weight.detach()
 
-        score_loss = torch.sum(snr_weight * (z_estimation + z) ** 2) / (
-            torch.sum(mask) * self.n_feats
-        )
+        use_snr_weighting = False
+        if use_snr_weighting:
+            snr_weight = alpha_t / (1.0 - alpha_t + 1e-5)
+            snr_weight = torch.clamp(snr_weight, max=3.0)
+            snr_weight = snr_weight.detach()
+            score_loss = torch.sum(snr_weight * (z_estimation + z) ** 2) / (
+                torch.sum(mask) * self.n_feats
+            )
+        else:
+            score_loss = torch.sum((z_estimation + z) ** 2) / (
+                torch.sum(mask) * self.n_feats
+            )
+
         x_hat = (xt - torch.sqrt(1 - alpha_t) * z_estimation) / torch.sqrt(alpha_t)
-
         rec_loss = l1_loss(x_hat, x0, reduction="mean")
 
         return score_loss, rec_loss
